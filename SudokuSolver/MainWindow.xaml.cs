5392d86a35f56c2e526c8ab4b2c80739b2aa2447
Jordy <Jordy@Z87X-UD4>
2021-07-13 19:34:09 +0200

    Add project files.

---
+++ SudokuSolver/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SudokuSolver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 81; i++)
+            {
+                TextBox textBox = new TextBox();
+                textBox.Text = string.Empty;
+                textBox.Width = 60;
+                textBox.Height = 60;
+                textBox.VerticalContentAlignment = VerticalAlignment.Center;
+                textBox.HorizontalContentAlignment = HorizontalAlignment.Center;
+                textBox.FontSize = 36;
+                textBox.FontWeight = FontWeights.Bold;
+                Sudoku.Children.Add(textBox);
+            }
+        }
+
+        private void btnSolve_Click(object sender, RoutedEventArgs e)
+        {            
+            Opgave opgave = new Opgave(Sudoku);
+            Solve();
+                        
+        }
+
+        private bool Validate(int row, int column, int number)
+        {
+            bool result = true;
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (Opgave.opgaveArray[row, i] == number)
+                {
+                    result = false;
+                }
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (Opgave.opgaveArray[i, column] == number)
+                {
+                    result = false;
+                }
+            }
+
+            var x0 = (column / 3) * 3;
+            var y0 = (row / 3) * 3;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (Opgave.opgaveArray[y0 + i, x0 + j] == number)
+                    {
+                        result = false;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private void Solve()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (Opgave.opgaveArray[i,j] == 0)
+                    {
+                        for (int k = 1; k < 10; k++)
+                        {
+                            if (Validate(i,j,k))
+                            {
+                                Opgave.opgaveArray[i, j] = k;
+                                Solve();
+                                Opgave.opgaveArray[i, j] = 0;
+                            }
+                        }
+                        return;
+                    }
+                }
+            }
+
+            int rows = 0;
+            int columns = 0;
+
+            foreach (TextBox textBox in Sudoku.Children)
+            {
+                textBox.Text = Opgave.opgaveArray[rows, columns].ToString();
+                columns++;
+                if (columns == 9)
+                {
+                    columns = 0;
+                    rows++;
+                }
+            }
+
+        }
+    }
+}
---
+++ SudokuSolver/Opgave.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SudokuSolver
+{
+    public class Opgave
+    {
+        public static int[,] opgaveArray = new int[9, 9];
+        public Opgave(WrapPanel sudoku)
+        {
+            int rows = 0;
+            int columns = 0;
+
+            foreach (TextBox item in sudoku.Children)
+            {
+                int number;
+                if (!int.TryParse(item.Text, out number))
+                {
+                    number = 0;
+                    item.Foreground = Brushes.DarkBlue;
+                }
+                else
+                {
+                    number = int.Parse(item.Text);
+                }
+                if (columns == 9)
+                {
+                    columns = 0;
+                    rows++;
+                }
+                opgaveArray[rows, columns] = number;
+                columns++;
+            }
+        }
+    }
+}
